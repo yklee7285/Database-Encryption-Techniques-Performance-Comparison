eb30886c821ee0de6cf1aaf18b942dd5a91e543d
Lee Yee Kang <75201065+yklee7285@users.noreply.github.com>
2025-12-25 22:51:33 +0800

    Change VARCHAR(12) to VARCHAR(255) for identification_card_no column
    VARCHAR(12) does not work as encrypted value exceeds 12 characters

--- create_table.sql
+++ create_table.sql
@@ -23,7 +23,7 @@ CREATE TABLE patients_app_encryption (
     patient_id INTEGER PRIMARY KEY,
     name VARCHAR(100),
     date_of_birth DATE,
-    identification_card_no VARCHAR(12),
+    identification_card_no VARCHAR(255),
     medical_condition VARCHAR(255),
     prescription VARCHAR(255)
 );
