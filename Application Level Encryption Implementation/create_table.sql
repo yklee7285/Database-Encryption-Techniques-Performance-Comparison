6cd4fe8c5e87b5e65e831e47d9542e11405b15c8
Lee Yee Kang <75201065+yklee7285@users.noreply.github.com>
2025-12-28 22:55:14 +0800

    Add SQL code for creating table for application level implementation
    Run this code in PostgreSQL

---
+++ Application Level Encryption Implementation/create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE patients_app_encryption (
+    patient_id INTEGER PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    identification_card_no VARCHAR(255),
+    medical_condition VARCHAR(255),
+    prescription VARCHAR(255)
+);
